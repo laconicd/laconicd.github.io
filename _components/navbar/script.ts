312aba814c464c708ab3653428909b9110666a48
laconicd <laconicd@gmail.com>
2026-02-25 07:55:00 +0900

    wip

--- _components/navbar/script.ts
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const popover = document.getElementById("index-popover");
-  if (!popover) return;
-
-  popover.querySelectorAll("a[href]")?.forEach((link) => {
-    link.addEventListener("htmx:configRequest", () => popover.hidePopover());
-  });
-});
