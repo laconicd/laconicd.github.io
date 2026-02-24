1b3d4cc96bcffefba234ac1d068dcb253dd8140d
laconicd <laconicd@users.noreply.github.com>
2026-02-24 22:55:23 +0000

    wip 312aba814c464c708ab3653428909b9110666a48

--- script.js
+++ script.js
@@ -1,3 +1,2 @@
-document.addEventListener("DOMContentLoaded",()=>{let e=document.getElementById("index-popover");e&&e.querySelectorAll("a[href]")?.forEach(t=>{t.addEventListener("htmx:configRequest",()=>e.hidePopover())})});
 
 //# sourceMappingURL=./script.js.map
